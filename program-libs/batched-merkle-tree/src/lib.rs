7ce6947d6936138bcc24b685c209c9e9f4c617c1
ananas-block <jorrit@lightprotocol.com>
2025-04-25 19:31:35 +0100

    cleanup

--- program-libs/batched-merkle-tree/src/lib.rs
+++ program-libs/batched-merkle-tree/src/lib.rs
@@ -18,11 +18,7 @@ use borsh::{BorshDeserialize, BorshSerialize};
 use pinocchio::{
     account_info::AccountInfo, msg, pubkey::Pubkey, sysvars::rent::Rent, sysvars::Sysvar,
 };
-// // Solana program imports for non-pinocchio builds (default)
-// #[cfg(not(feature = "pinocchio"))]
-// use solana_program::{
-//     account_info::AccountInfo, msg, pubkey::Pubkey, sysvar::rent::Rent, sysvar::Sysvar,
-// };
+// Solana program imports for non-pinocchio builds (default)
 #[cfg(not(feature = "pinocchio"))]
 pub(crate) use {
     solana_account_info::AccountInfo,
